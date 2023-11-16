1cc5aa00557ff342ef55f8506092a751e875d256
Kale <kale.rusaw@mail.mcgill.ca>
2023-11-16 10:58:29 -0500

    print statements

--- path_to_instructions.py
+++ path_to_instructions.py
@@ -1,12 +1,3 @@
-import math
-import time
-from utils.brick import Motor, wait_ready_sensors, EV3ColorSensor
-
-CM = Motor("B")
-KM = Motor("C")
-# CM.reset_encoder()
-# KM.reset_encoder()
-
 #thisfilewaswrittenbykALE
 
 d = 0
@@ -26,53 +17,85 @@ def path_to_instruction(input):
     for i in range(len(input - 1)):
         if input[i][0] < input[i+1][0] and input[i][1] == input[i+1][1] and d == 0: #travel in +x direction, already facing +x direction
             forward()
+            d = 0
+            print("+x from +x")
         elif input[i][0] < input[i+1][0] and input[i][1] == input[i+1][1] and d != 0: #travel in +x direction, not already facing +x direction
             if d == 1:
                 turn_right()
                 forward()
+                d = 0
+                print("+x from +y")
             elif d == 2:
                 turn_right()
                 turn_right()
-                forward()
+                forward() 
+                d = 0
+                print("+x from -x")
             elif d == 3:
                 turn_left()
                 forward()
+                d = 0
+                print("+x from -y")
         elif input[i][0] == input[i+1][0] and input[i][1] < input[i+1][1] and d == 1: #travel in +y direction, already facing +y direction
             forward()
+            d = 1
+            print("+y from +y")
         elif input[i][0] == input[i+1][0] and input[i][1] < input[i+1][1] and d != 1: #travel in +y direction, not already facing +y direction
             if d == 0:
                 turn_left()
                 forward()
+                d = 1
+                print("+y from +x")
             elif d == 2:
                 turn_right()
                 forward()
+                d = 1
+                print("+y from -x")
             elif d == 3:
                 turn_right()
                 turn_right()
                 forward()
+                d = 1
+                print("+y from -y")
         elif input[i][0] > input[i+1][0] and input[i][1] == input[i+1][1] and d == 2: #travel in -x direction, already facing -x direction
             forward()
+            d = 2
+            print("-x from -x")
         elif input[i][0] > input[i+1][0] and input[i][1] == input[i+1][1] and d != 2: #travel in -x direction, not already facing -x direction
             if d == 0:
                 turn_right()
                 turn_right()
-                forward()
+                forward() 
+                d = 2
+                print("-x from +x")
             if d == 1:
                 turn_left()
                 forward()
+                d = 2
+                print("-x from +y")
             if d == 3:
                 turn_right()
                 forward()
+                d = 2
+                print("-x from -y")
         elif input[i][0] == input[i+1][0] and input[i][1] > input[i+1][1] and d == 3: #travel in -y direciton, already facing -y direction
             forward()
+            d = 3
+            print("-y from -y")
         elif input[i][0] == input[i+1][0] and input[i][1] > input[i+1][1] and d != 3: #travel in -y direciton, not already facing -y direction
             if d == 0:
                 turn_right()
                 forward()
+                d = 3
+                print("-y from +x")
             elif d == 1: 
                 turn_right()
                 turn_right()
                 forward()
+                d = 3
+                print("-y from +y")
             elif d== 2:
                 turn_left()
                 forward()
+                d =3
+                print("-y from -x")
